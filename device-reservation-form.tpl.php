5b3a2fe793abe1da9b2353361b98e7358fd51fd9
hanmant-sapkal <hanmant-sapkal@users.noreply.github.com>
2015-01-20 16:20:09 +0530

    Create device-reservation-form.tpl.php

---
+++ device-reservation-form.tpl.php
@@ -0,0 +1,5 @@
+<div id="reservation-form-wrapper">
+      <?php print drupal_render($form['firstname']);?>
+      <?php print drupal_render($form['lastname']);?>
+      <?php print drupal_render($form); // dont remove this line, this will print all remaing hidden elements?>
+</div>
